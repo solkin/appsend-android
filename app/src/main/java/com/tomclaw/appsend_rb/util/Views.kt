32f62bee10081c5db849e572721e5aae70b2efa7
Igor Solkin <inbox@tomclaw.com>
2020-02-28 00:11:38 +0300

    Add helpful views extensions

--- app/src/main/java/com/tomclaw/appsend_rb/util/Views.kt
+++ app/src/main/java/com/tomclaw/appsend_rb/util/Views.kt
@@ -2,11 +2,17 @@ package com.tomclaw.appsend_rb.util
 
 import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
+import android.text.Editable
+import android.text.TextUtils
+import android.text.TextWatcher
 import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.view.ViewPropertyAnimator
 import android.view.animation.AccelerateDecelerateInterpolator
+import android.widget.EditText
+import android.widget.TextView
+import com.jakewharton.rxrelay2.Relay
 
 fun View?.toggle() {
     if (this?.visibility == VISIBLE) hide() else show()
@@ -20,6 +26,32 @@ fun View?.show() {
 
 fun View?.hide() {
     this?.visibility = GONE
+}
+
+fun TextView.bind(value: String?) {
+    if (TextUtils.isEmpty(value)) {
+        visibility = GONE
+        text = ""
+    } else {
+        visibility = VISIBLE
+        text = value
+    }
+}
+
+fun View.clicks(relay: Relay<Unit>) {
+    setOnClickListener { relay.accept(Unit) }
+}
+
+fun EditText.changes(handler: (String) -> Unit) {
+    addTextChangedListener(object : TextWatcher {
+        override fun afterTextChanged(s: Editable) {}
+
+        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+            handler.invoke(s.toString())
+        }
+    })
 }
 
 fun View.showWithAlphaAnimation(
