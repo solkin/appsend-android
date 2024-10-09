7c80dc1166d6db92d6fe2f2f0929779f2219283d
Igor Solkin <inbox@tomclaw.com>
2024-10-10 02:17:53 +0300

    Remove legacy permission helper methods

--- app/src/main/java/com/tomclaw/appsend_rb/util/PreferenceHelper.java
+++ app/src/main/java/com/tomclaw/appsend_rb/util/PreferenceHelper.java
@@ -13,58 +13,13 @@ import com.tomclaw.appsend_rb.R;
  */
 public class PreferenceHelper {
 
-    public static final boolean IS_DONATE_ENABLED = false;
-
     public static boolean isDarkTheme(Context context) {
         return getBooleanPreference(context, R.string.pref_dark_theme, R.bool.pref_dark_theme_default);
-    }
-
-    public static boolean isShowSystemApps(Context context) {
-        return getBooleanPreference(context, R.string.pref_show_system, R.bool.pref_show_system_default);
-    }
-
-    public static boolean isRunnableOnly(Context context) {
-        return getBooleanPreference(context, R.string.pref_runnable, R.bool.pref_runnable_default);
-    }
-
-    public static String getSortOrder(Context context) {
-        return getStringPreference(context, R.string.pref_sort_order, R.string.pref_sort_order_default);
-    }
-
-    public static boolean isShowResponsibilityDenial(Context context) {
-        return getBooleanPreference(context, R.string.pref_responsibility_denial, R.bool.pref_responsibility_denial_default);
-    }
-
-    public static void setShowResponsibilityDenial(Context context, boolean value) {
-        setBooleanPreference(context, R.string.pref_responsibility_denial, value);
-    }
-
-    public static long getCountTime(Context context) {
-        return Long.parseLong(getStringPreference(context, R.string.pref_count_time, R.string.pref_count_time_default));
-    }
-
-    public static void setCountTime(Context context, long value) {
-        setStringPreference(context, R.string.pref_count_time, String.valueOf(value));
     }
 
     private static boolean getBooleanPreference(Context context, int preferenceKey, int defaultValueKey) {
         return getSharedPreferences(context).getBoolean(context.getResources().getString(preferenceKey),
                 context.getResources().getBoolean(defaultValueKey));
-    }
-
-    private static void setBooleanPreference(Context context, int preferenceKey, boolean value) {
-        getSharedPreferences(context).edit().putBoolean(context.getResources().getString(preferenceKey),
-                value).apply();
-    }
-
-    private static String getStringPreference(Context context, int preferenceKey, int defaultValueKey) {
-        return getSharedPreferences(context).getString(context.getResources().getString(preferenceKey),
-                context.getResources().getString(defaultValueKey));
-    }
-
-    private static void setStringPreference(Context context, int preferenceKey, String value) {
-        getSharedPreferences(context).edit().putString(context.getResources().getString(preferenceKey),
-                value).apply();
     }
 
     private static SharedPreferences getSharedPreferences(Context context) {
